f646bfab6e2dd8d39bc866b68985a718bb215e02
Arbuleac <dev@arbuleac.com>
2017-02-20 13:18:25 +0200

    some fixes

--- sqldelight-gradle-plugin/src/test/integration/src/main/java/com/squareup/sqldelight/integration/SqliteKeywords.java
+++
@@ -1,7 +0,0 @@
-package com.squareup.sqldelight.integration;
-
-import com.google.auto.value.AutoValue;
-
-@AutoValue public abstract class SqliteKeywords implements SqliteKeywordsModel {
-  public static final Factory<SqliteKeywords> FACTORY = new Factory<>(AutoValue_SqliteKeywords::new);
-}
